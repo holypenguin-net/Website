c10f4be92eb3f5095015f1161dc859b3f1713099
Omicrom757 <rick.ziegler@protonmail.com>
2022-12-09 09:51:35 +0000

    Add navbar on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,85 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import "./globals.css";
+import style from "./navbar.module.css";
+import logo from "../public/icons/dark/logo_dark.svg";
+import nav_icon from "../public/icons/dark/nav_icon_dark.svg";
+import server from "../public/icons/dark/server_dark.svg";
+import games from "../public/icons/dark/game_dark.svg";
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
+  let image_size = 200;
+
   return (
     <html>
       <head />
       <body>
-        <div>
-            <Link href='/'>Home</Link>
-            <Link href='/games'>Games</Link>
-            <Link href='/register'>Register</Link>
-            <Link href='/login'>Login</Link>        
-        </div>
+        <nav className={style.navbar}>
+          <ul className={style.navbar_nav}>
+
+          <li className={style.logo}>
+            <Link href="/" className={style.nav_link}>
+              <span className={`${style.link_text} ${style.logo_text}`}>Home</span>
+              <Image src={nav_icon} alt='Logo'/>
+            </Link>
+          </li>
+
+          <li className={style.nav_item}>
+            <Link href="/games" className={style.nav_link}>
+              <Image src={logo} alt='Home' width={image_size} height={image_size}/>
+              <span className={style.link_text}>News</span>
+            </Link>
+          </li>
+
+          <li className={style.nav_item}>
+            <Link href="/games" className={style.nav_link}>
+              <Image src={server} alt='Home' width={image_size} height={image_size}/>
+              <span className={style.link_text}>Meine Server</span>
+            </Link>
+          </li>
+
+          <li className={style.nav_item}>
+            <Link href="/games" className={style.nav_link}>
+              <Image src={games} alt='Home' width={image_size} height={image_size}/>
+              <span className={style.link_text}>Games</span>
+            </Link>
+          </li>
+
+          <li className={style.nav_item}>
+            <Link href="/games" className={style.nav_link}>
+              <Image src={logo} alt='Home' width={image_size} height={image_size}/>
+              <span className={style.link_text}>Minecraft</span>
+            </Link>
+          </li>
+
+          <li className={style.nav_item}>
+            <Link href="/games" className={style.nav_link}>
+              <Image src={logo} alt='Home' width={image_size} height={image_size}/>
+              <span className={style.link_text}>Terraria</span>
+            </Link>
+          </li>
+
+          <div className={style.nav_last_item}>
+            <li className={style.nav_item}>
+              <Link href="/login" className={style.nav_link}>
+                <Image src={logo} alt='Home' width={image_size} height={image_size}/>
+                <span className={style.link_text}>Discord</span>
+              </Link>
+            </li>
+
+            <li className={style.nav_item}>
+              <Link href="/login" className={style.nav_link}>
+                <Image src={logo} alt='Home' width={image_size} height={image_size}/>
+                <span className={style.link_text}>Login</span>
+              </Link>
+            </li>
+          </div>
+
+          </ul>
+        </nav>
+          <main>
           {children}
+          </main>
       </body>
     </html>
   );
