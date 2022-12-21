9272a7309450833c241872d402a35b1798b7b242
holypenguin-de <sveny.ziegler@gmail.com>
2022-12-21 16:21:02 +0100

    Add Register API

--- lib/classes/user.ts
+++ lib/classes/user.ts
@@ -1,6 +1,6 @@
 import type {Client} from 'pg';
 import { Token } from './token';
-import { compare } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 
 export type userType = {
     usr_ID_PK?: number | undefined,
@@ -40,61 +40,95 @@ export class User{
         });
     };
 
+    // Register a new User
+    public async register(){
+        return new Promise(async (resolve, reject) =>{
+            if(this.user){
+                // prepare Statement + Values + Hash Password
+                await hash((this.user.usr_Password as string), 12)
+                    .then((pwHash) => {
+                        // @ts-ignore
+                        this.user.usr_Password = pwHash;
+                    })
+                    .catch((e) => {
+                        reject(e);
+                    });
+                const stmt = 'INSERT INTO "User" (usr_Nickname, usr_Email, usr_Password, usr_Admin) VALUES ($1, $2, $3, $4);';
+                const values = [this.user.usr_Nickname, this.user.usr_Email, this.user.usr_Password, this.user.usr_Admin];
+
+                // create new user
+                await this.client.query(stmt, values)
+                    .then(() => {
+                       resolve({"msg": "Success"});
+                    })
+                    .catch((e) => {
+                       reject(e); 
+                    });
+            }else{
+                reject();
+            }
+        });
+    }
+
     // Log a user in
     public async login(keepLogin: boolean){
         return new Promise(async (resolve, reject) =>{
-            // prepare Statment + Values
-            const stmt = 'SELECT * FROM "User" WHERE usr_Nickname = $1 OR usr_Email = $2;';
-            const values = [this.user?.usr_Nickname, this.user?.usr_Email];
-            let info: object;
-            let mail: string;
-            let nickname: string;
+            if(this.user){
+                // prepare Statment + Values
+                const stmt = 'SELECT * FROM "User" WHERE usr_Nickname = $1 OR usr_Email = $2;';
+                const values = [this.user.usr_Nickname, this.user.usr_Email];
+                let info: object;
+                let mail: string;
+                let nickname: string;
 
-            // Query user by Nickname or Email
-            await this.client.query(stmt, values)
-                .then(async (res) => {
-                    if(res.rowCount != 0 && this.user?.usr_Password){
+                // Query user by Nickname or Email
+                await this.client.query(stmt, values)
+                    .then(async (res) => {
+                        if(res.rowCount != 0 && this.user?.usr_Password){
+                            
+                            // Compare given password with hash in db
+                            compare(this.user.usr_Password, res.rows[0].usr_password)
+                                .then(async (result)=> {
+                                    // If password is correct
+                                    if(result){
+                                        info = {
+                                            usr_ID_FK: res.rows[0].usr_id_pk
+                                        };
+                                        mail = res.rows[0].usr_email;
+                                        nickname = res.rows[0].usr_nickname;
+
+                                        // gnenerate a new session-token
+                                        const token = new Token(this.client, info, keepLogin);
+                                        await token.generate()
+                                            .then((res) => {
+                                                // Build the Response
+                                                // @ts-ignore
+                                                res.rows[0].usr_email = mail;
+                                                // @ts-ignore
+                                                res.rows[0].usr_nickname = nickname;
+                                                resolve(res);
+                                            })
+                                            .catch((e) => {
+                                                reject(e);
+                                            })
+                                    }else{
+                                        resolve({});
+                                    }
+                                })
+                                .catch((e) => {
+                                    reject(e);
+                                });
                         
-                        // Compare given password with hash in db
-                        compare(this.user.usr_Password, res.rows[0].usr_password)
-                            .then(async (result)=> {
-                                // If password is correct
-                                if(result){
-                                    info = {
-                                        usr_ID_FK: res.rows[0].usr_id_pk
-                                    };
-                                    mail = res.rows[0].usr_email;
-                                    nickname = res.rows[0].usr_nickname;
-
-                                    // gnenerate a new session-token
-                                    const token = new Token(this.client, info, keepLogin);
-                                    await token.generate()
-                                        .then((res) => {
-                                            // Build the Response
-                                            // @ts-ignore
-                                            res.rows[0].usr_email = mail;
-                                            // @ts-ignore
-                                            res.rows[0].usr_nickname = nickname;
-                                            resolve(res);
-                                        })
-                                        .catch((e) => {
-                                            reject(e);
-                                        })
-                                }else{
-                                    resolve({});
-                                }
-                            })
-                            .catch((e) => {
-                                reject(e);
-                            });
-                    
-                    }else{
-                        resolve(res);
-                    }
-                })
-                .catch((e) => {
-                   reject(e); 
-                });
+                        }else{
+                            resolve(res);
+                        }
+                    })
+                    .catch((e) => {
+                       reject(e); 
+                    });
+            }else{
+                reject();
+            }
         });
     };
 }
---
+++ pages/api/user/register.ts
@@ -0,0 +1,52 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import db from '../../../lib/functions/db';
+import type { apiResponseMessage } from "../../../lib/types/api";
+import { User } from '../../../lib/classes/user';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<apiResponseMessage>){
+    let apiRes: apiResponseMessage;
+    if(req.method === "POST"){
+        // Prepare DB
+        const client = db();
+
+        // Prepare Classes
+        const user = new User(client);
+
+        user.user = {
+            usr_Nickname: req.body.usr_Nickname,
+            usr_Email: req.body.usr_Email,
+            usr_Password: req.body.usr_Password,
+            usr_Admin: false
+        };
+
+        await user.register()
+            .then((queryRes) => {
+                console.log(queryRes);
+                apiRes = {
+                    statusName: 'Ok',
+                    statusCode: 200,
+                    isError: false,
+                    msg: (queryRes as object)
+                }
+                res.status(200).json(apiRes);
+            })
+            .catch((e) => {
+                apiRes = {
+                    statusName: 'Internal Server Error',
+                    statusCode: 500,
+                    isError: true,
+                    msg: e
+                }
+                res.status(500).json(apiRes);
+            })
+        
+    }else{
+        apiRes = {
+            statusName: 'Bad Request',
+            statusCode: 405,
+            isError: true,
+            msg: {"error": "Invalid Method"}
+        }
+        res.status(405).json(apiRes);
+    }
+};
