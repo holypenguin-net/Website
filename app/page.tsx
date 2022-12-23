a25ee1686841338739d3b02b370cb015edefaea0
Svenum <s.ziegler@holypenguin.net>
2022-12-24 00:25:37 +0100

    Test autodeploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function home(){
     return(
         <>
             <h1>Home</h1>
-            <h2>THIS IS A DEV VERSION OF THE WEBSITE</h2>
+            <h2>THIS IS A DEV VERSION OF THIS WEBPAGE</h2>
             <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
 
 
