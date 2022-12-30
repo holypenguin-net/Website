d18498618e2426759602a91c3c72e0207dc56cef
Omicrom757 <rick.ziegler@protonmail.com>
2022-12-30 10:43:06 -0500

    update login and add register

--- app/discord/page.tsx
+++ app/discord/page.tsx
@@ -1,8 +1,14 @@
+import { HolyForm } from "../../lib/components/holyform"
+
 export default function discord(){
     return(
         <>
+        <HolyForm header = "Discord">
+            
+            <input type={"text"} placeholder="placeholder"></input>
             <h1>Discord</h1>
             <p> some text </p>
+        </HolyForm>
         </>
     )
 }
