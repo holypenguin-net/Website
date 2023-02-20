367afa3e6603e8fd25887d2e8e8863827a7dc924
Svenum <s.ziegler@holypenguin.net>
2023-02-20 17:05:11 +0100

    Fix bugs

--- app/logout/page.tsx
+++ app/logout/page.tsx
@@ -4,5 +4,9 @@ import { deleteCookie } from "cookies-next";
 export default function Logout(){
   deleteCookie('session');
   document.location.href = "/";
-  return;
+  return(
+    <>
+
+    </>
+  );
 }
